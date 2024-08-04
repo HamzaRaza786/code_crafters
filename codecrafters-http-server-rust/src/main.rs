dc5e07abef0f7883f0587bb543b47a34eadb139b
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-08-04 17:17:48 +0500

    Read User-Agent Header

--- codecrafters-http-server-rust/src/main.rs
+++ codecrafters-http-server-rust/src/main.rs
@@ -8,27 +8,31 @@ use std::{
 fn handle_client(mut stream: TcpStream) {
     // Sending a response to the client
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let mut lines = buf_reader.lines();
+    let request_line = lines.next().unwrap().unwrap();
     if request_line == "GET / HTTP/1.1" {
         let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
         stream.write_all(response.as_bytes()).unwrap();
     } else if request_line.contains("GET /echo") {
-        let string_route = request_line
-            .split('/')
-            .collect::<Vec<_>>()
-            .get(2)
-            .unwrap()
-            .split(' ')
-            .collect::<Vec<_>>()
-            .get(0)
-            .unwrap()
-            .clone();
-        println!("{}", string_route);
+        let string_route = request_line.split_whitespace().nth(1).unwrap();
+        let string_in_the_route = string_route.trim_start_matches("/echo/");
 
         let response = format!(
             "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n Content-Length: {}\r\n{}",
-            string_route.len(),
-            string_route
+            string_in_the_route.len(),
+            string_in_the_route
+        );
+        println!("{}", response);
+        stream.write_all(response.as_bytes()).unwrap();
+    } else if request_line.contains("GET /user-agent") {
+        let user_agent = lines.nth(2).unwrap().unwrap();
+        println!("{}", user_agent);
+        let string_in_the_route = user_agent.trim_start_matches("USER-AGENT: ");
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n Content-Length: {}\r\n{}",
+            string_in_the_route.len(),
+            string_in_the_route
         );
         println!("{}", response);
         stream.write_all(response.as_bytes()).unwrap();
